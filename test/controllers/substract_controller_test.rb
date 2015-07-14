5f5ac6e9deaa57e7574726731397ab130029488c
yilmazalican <yilmazalican@outlook.com>
2015-07-14 06:47:57 +0300

    Added substraction controller test

--- test/controllers/substract_controller_test.rb
+++ test/controllers/substract_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SubstractControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "SubstractControllerTest" do
+     params = {'a' => 2, 'b' => 1}
+     json_headers = {"Content-Type" => "application/json",
+                     "Accept" => "application/json"}
+
+     uri = URI.parse('http://localhost:3000/substract')
+     http = Net::HTTP.new(uri.host, uri.port)
+
+     response = http.post(uri.path, params.to_json, json_headers)
+     data = JSON.parse(response.body)
+
+     if data["res"] == 1
+       assert true
+     else
+       assert false
+     end
+   end
 end
