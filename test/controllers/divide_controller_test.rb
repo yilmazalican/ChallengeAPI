dd6ad397e1c51c610bb9039079e5d5a95f9f06f4
yilmazalican <yilmazalican@outlook.com>
2015-07-14 06:25:18 +0300

    AddedDivideController Test

--- test/controllers/divide_controller_test.rb
+++ test/controllers/divide_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class DivideControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "DivideControllerTest" do
+
+  params = {'a' => 1 , 'b' => 1}
+  json_headers = {"Content-Type" => "application/json",
+                  "Accept" => "application/json"}
+
+  uri = URI.parse('http://localhost:3000/divide')
+  http = Net::HTTP.new(uri.host, uri.port)
+
+  response = http.post(uri.path, params.to_json, json_headers)
+  data = JSON.parse(response.body)
+
+  if data["res"] == 1
+    assert true
+  else
+    assert false
+end
+end
 end
