09ef6d2ef41d6ae7a7d6c23f5bb332fd959f38ca
yilmazalican <yilmazalican@outlook.com>
2015-07-14 06:46:40 +0300

    Added next prime controller test

--- test/controllers/nextprime_controller_test.rb
+++ test/controllers/nextprime_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class NextprimeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "NextprimeControllerTest" do
+
+     params = {'n' => 2}
+     json_headers = {"Content-Type" => "application/json",
+                     "Accept" => "application/json"}
+
+     uri = URI.parse('http://localhost:3000/nextprime')
+     http = Net::HTTP.new(uri.host, uri.port)
+
+     response = http.post(uri.path, params.to_json, json_headers)
+     data = JSON.parse(response.body)
+
+     if data["result"] == 3
+       assert true
+     else
+       assert false
+   end
+   end
 end
