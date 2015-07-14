59aed3598d67eaf4cba75439054bcb479caa6a54
yilmazalican <yilmazalican@outlook.com>
2015-07-14 06:22:44 +0300

    Added Digitofpi test file

--- test/controllers/digitofpi_controller_test.rb
+++ test/controllers/digitofpi_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class DigitofpiControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #Assertion for if the model does its job correct with the rails' model.
+  #Please do not forget to run the WEBBRICK server from http://localhost:3000/digitofpi
+   test "DigitofPiTest" do
+
+
+     params = {'n' => 1}
+     json_headers = {"Content-Type" => "application/json",
+                     "Accept" => "application/json"}
+
+     uri = URI.parse('http://localhost:3000/digitofpi')
+     http = Net::HTTP.new(uri.host, uri.port)
+
+     response = http.post(uri.path, params.to_json, json_headers)
+     data = JSON.parse(response.body)
+p data["res"]
+     if data["res"] == 1.to_s
+       assert true
+     else
+       assert false
+  end
+
+  end
 end
