52204bcfbd83e420a8b24b48d0935232b2595367
yilmazalican <yilmazalican@outlook.com>
2015-07-14 06:42:58 +0300

    Added Multiplycontroller test

--- test/controllers/multiply_controller_test.rb
+++ test/controllers/multiply_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MultiplyControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "Multiplycontrollertest" do
+     params = {'a' => 2, 'b' => 3 }
+     json_headers = {"Content-Type" => "application/json",
+                     "Accept" => "application/json"}
+
+     uri = URI.parse('http://localhost:3000/multiply')
+     http = Net::HTTP.new(uri.host, uri.port)
+
+     response = http.post(uri.path, params.to_json, json_headers)
+     data = JSON.parse(response.body)
+   p data["res"]
+     if data["res"] == 6
+       assert true
+     else
+       assert false
+   end
+   end
 end
