15ffe98eef33dc296e51206f8371c6d3d06988ec
yilmazalican <yilmazalican@outlook.com>
2015-07-14 06:17:04 +0300

    refactored add_controller_test.rb and added allsum test

--- test/controllers/add_controller_test.rb
+++ test/controllers/add_controller_test.rb
@@ -5,11 +5,17 @@ class AddControllerTest < ActionController::TestCase
 #Assertion for if the model does its job correct with the rails' model.
 #Please do not forget to run the WEBBRICK server from http://localhost:3000/add
    test "GettingResponseTrue" do
-     uri = URI.parse("http://localhost:3000/add")
-     response = Net::HTTP.post_form(uri, {"a" => "1", "b" => "2"})
+     params = {'a' => 1, 'b' => 2}
+     json_headers = {"Content-Type" => "application/json",
+                     "Accept" => "application/json"}
+
+     uri = URI.parse('http://localhost:3000/add')
+     http = Net::HTTP.new(uri.host, uri.port)
+
+     response = http.post(uri.path, params.to_json, json_headers)
      data = JSON.parse(response.body)
 
-     if data["res"] == "12"
+     if data["res"] == 3
        assert true
      else
        assert false
--- test/controllers/allsum_controller_test.rb
+++ test/controllers/allsum_controller_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
+
+#Assertion for if the model does its job correct with the rails' model.
+#Please do not forget to run the WEBBRICK server from http://localhost:3000/allsum
 class AllsumControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     params = {'n' => 3}
+     json_headers = {"Content-Type" => "application/json",
+                     "Accept" => "application/json"}
+
+     uri = URI.parse('http://localhost:3000/allsum')
+     http = Net::HTTP.new(uri.host, uri.port)
+
+     response = http.post(uri.path, params.to_json, json_headers)
+     data = JSON.parse(response.body)
+
+     if data["res"] == 6
+       assert true
+     else
+       assert false
+  end
+   end
 end
